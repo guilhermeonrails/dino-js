790c660dee81da57a54ac02c223a00ae30bedcfc
guilhermelima <guilhermelimaruby@gmail.com>
2023-12-11 14:48:55 -0300

    first commit

--- script.js
+++ script.js
@@ -1,55 +1,127 @@
-var imgChao;
-var posicaoChaoX = 0;
-var imgFundo;
-var posicaoFundoX = 0;
-var imgsDino = [];
-var imgDinoAtual = 0;
+var floorImg;
+var floorPositionX = 0;
+var backgroundImg;
+var backgroundPositionX = 0;
+var dinoImgs = [];
+var currentDinoImg = 0;
+var cactusImgs = []
+var posXCactus
+
+var jumping = false;
+var jumpStartY;
+var ySpeed;
+var gravity = 1
+var posYDino = 0.67
+var velocidade = 15
 
 function preload() {
-    imgChao = loadImage('assets/chao.png');
-    imgFundo = loadImage('assets/bg.jpeg');
+    floorImg = loadImage('assets/floor.png');
+    backgroundImg = loadImage('assets/bg.jpeg');
+    
+    for (var i = 1; i <= 2; i++) {
+        cactusImgs.push(loadImage(`assets/cactus/cactus${i}.png`))
+    }
+
     for (var i = 1; i <= 8; i++) {
-        imgsDino.push(loadImage(`assets/dino/${i}.png`));
+        dinoImgs.push(loadImage(`assets/dino/${i}.png`));
     }
 }
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    inicioPuloY = windowHeight * 0.57;
+    jumpStartY = windowHeight * posYDino
+    ySpeed = 0 
+    valorDoPulo = (-15 /windowHeight) * (windowHeight *2)
+    posXCactus = windowWidth * 1.2
 }
 
 function draw() {
-    moverFundo();
-    moverChao();
-    animarDino();
+    background(0)
+    moveBackground();
+    moveFloor();
+    animateCactus();
+    animateDino();
+    applyGravity();
 }
 
-function animarDino() {
-    image(imgsDino[imgDinoAtual], windowWidth * 0.1, windowHeight * 0.57, windowHeight * 0.32, windowHeight * 0.3);
-
-    if (frameCount % 6 === 0) {
-        imgDinoAtual = (imgDinoAtual + 1) % imgsDino.length;
+function verificaColisao() {
+    if (collideRectRect(img1X, img1Y, img1.width, img1.height, img2X, img2Y, img2.width, img2.height)) {
+        noLoop();
     }
 }
 
-function moverChao() {
-    image(imgChao, posicaoChaoX, 0, windowWidth, windowHeight);
-    image(imgChao, posicaoChaoX + width, 0, windowWidth, windowHeight);
+function animateCactus() {
+    var posY = windowHeight * 0.65 
+    var tamanho = posY / 3   
+    image(cactusImgs[1], posXCactus, posY, tamanho, tamanho)
 
-    posicaoChaoX -= 5;
-    if (posicaoChaoX < -width) {
-        posicaoChaoX = 0;
+    posXCactus -= velocidade
+
+    if (posXCactus < -width) {
+        posXCactus = windowWidth * 1.2
+    }
+    
+}
+
+function animateDino() {
+    var spriteDino;
+    
+    if (!jumping) {
+        spriteDino = dinoImgs[currentDinoImg];
+    } else {
+        // Se está pulando, use a mesma sprite durante o salto
+        spriteDino = dinoImgs[0];
+    }
+    
+    image(spriteDino, windowWidth * 0.04, jumpStartY, windowWidth * 0.18, windowHeight * 0.2);
+
+    if (!jumping && frameCount % 6 === 0) {
+        currentDinoImg = (currentDinoImg + 1) % dinoImgs.length;
     }
 }
 
-function moverFundo() {
-    var posY = (windowHeight / 6 ) * -1;
-    image(imgFundo, posicaoFundoX, posY, windowWidth, windowHeight);
-    image(imgFundo, posicaoFundoX + width, posY, windowWidth, windowHeight);
+function moveFloor() {
+    var posY = windowHeight * .05
+    image(floorImg, floorPositionX,         posY, windowWidth, windowHeight);
+    image(floorImg, floorPositionX + width, posY, windowWidth, windowHeight);
 
-    posicaoFundoX -= 0.5;
-
-    if (posicaoFundoX < -width) {
-        posicaoFundoX = 0;
+    floorPositionX -= velocidade;
+    if (floorPositionX < -width) {
+        floorPositionX = 0;
     }
 }
+
+function moveBackground() {
+    image(backgroundImg, backgroundPositionX,         10, windowWidth, windowHeight *0.8);
+    image(backgroundImg, backgroundPositionX + width, 10, windowWidth, windowHeight *0.8);
+
+    backgroundPositionX -= 0.3;
+
+    if (backgroundPositionX < -width) {
+        backgroundPositionX = 0;
+    }
+}
+
+function applyGravity() {
+    ySpeed += gravity;
+    jumpStartY += ySpeed;
+
+    if (jumpStartY > height * posYDino) {
+      jumpStartY = height * posYDino;
+      jumping = false;
+    } else {
+      jumping = true;
+    }
+}
+
+function keyTyped() {
+    if (!jumping && key === ' ') {
+      ySpeed = valorDoPulo;
+    }
+  }
+
+function mouseClicked() {
+    if (!jumping) {
+      ySpeed = valorDoPulo;
+    }
+  }
